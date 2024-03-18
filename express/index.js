8302b011a1cc38342b70bb72c52acb85e441fb90
alexbinette-ets <alexandre.binette.2@ens.etsmtl.ca>
2024-03-18 09:55:27 -0400

    mongoose -> mongodb node driver

--- express/index.js
+++ express/index.js
@@ -1,10 +1,32 @@
 const express = require("express");
-const db = require("./db");
 const app = express();
 
 
-db.connect(app);
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// Replace the placeholder with your Atlas connection string
+const uri = "mongodb://mongodb_1:27017";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(5000, function () {
  console.log("Express écoute sur le port 5000!");
