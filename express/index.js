904e427c9a0ba35010c9e6ed95511116a9b4960e
ARTETS <alexis.roy-tessier.1@ens.etsmtl.ca>
2024-03-20 20:14:25 -0400

    Implémentation des routes API
    -Get date actuelle
    -Get calendrier (infos pour construction calendrier Frontend)
    -Get infos d'une journée (le click listener à implémenter en front end pour que ca inclue les parametres dans la requete), ça retourne d:
    [date, heure_debut, heure_fin, num_reservation, numero_table, nb_sieges, specification]

--- express/index.js
+++ express/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const routesEmploye = require('./routes/routesEmploye');
 const app = express();
 
 
@@ -19,18 +20,55 @@ async function run() {
     // Connect the client to the server (optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("TestAPI1erSprint").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    app.locals.db = client.db("TestAPI1erSprint");
+    app.listen(5000, function () {
+      console.log("Express écoute sur le port 5000!");
+     });
+      } 
+     catch (err) {
+      console.log("MongoDB connection unsuccessful, retry after 2 seconds.", err);
+      setTimeout(connectWithRetry, 2000);
   }
 }
+
+/*code que jai enleve
+finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();*/
+
+//on run la connection DB
 run().catch(console.dir);
 
+<<<<<<< Updated upstream
 app.listen(5000, function () {
  console.log("Express écoute sur le port 5000!");
 });
 app.get('/', function(req, res) {
   res.send('hello world');
 });
+=======
+
+//on attend que express ferme pour fermer la connection à DB
+process.on('SIGINT', async () => {
+  try {
+    // Fermez la connexion MongoDB lorsque l'application se termine
+    await client.close();
+    console.log('Connexion à MongoDB fermée.');
+    process.exit(0);
+  } catch (error) {
+    console.error('Erreur lors de la fermeture de la connexion à MongoDB:', error);
+    process.exit(1);
+  }});
+
+//middleware quon va utiliser plus tard pour authentification
+//pasfiniapp.use(express.json());
+app.use((req,res,next) => {
+console.log(req.path,req.method)
+next()
+})
+
+//routes
+app.use('/api/employe', routesEmploye);
+>>>>>>> Stashed changes
