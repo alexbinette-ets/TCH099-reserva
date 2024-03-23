82b91b5b30649e112e94db3920e7ec0a443b6c82
alexbinette-ets <alexandre.binette.2@ens.etsmtl.ca>
2024-03-22 20:14:51 -0400

    réparation scripts conversion, thx alexis

--- fichiers_db/importDB.bat
+++ fichiers_db/importDB.bat
@@ -1,4 +1,4 @@
 @echo off
 docker cp ../fichiers_db/ mongodb_1:/tmp/
-docker exec -it mongodb_1 sh -c "chmod +x /tmp/fichiers_db/mongoimport.sh && /tmp/fichiers_db/mongoimport.sh"
-
+rem docker exec -it mongodb_1 sh -c "chmod +x /tmp/fichiers_db/mongoimport.sh && /tmp/fichiers_db/mongoimport.sh"
+docker exec -it mongodb_1 sh -c "cd /tmp/fichiers_db/ && tr -d '\r' < mongoimport.sh > mongoimport.sh.tmp && mv mongoimport.sh.tmp mongoimport.sh && bash ./mongoimport.sh"
