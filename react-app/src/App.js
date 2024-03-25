62cc799ad4fb6a8c804f6f980aee2f322ab51b76
DonaldVu2511 <donald.vu.1@ens.etsmtl.ca>
2024-03-25 13:11:20 -0400

    Finished Calendrier

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,39 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Calendrier from './Calendrier';
-import Reservation from './Reservation';
-
-
 function App() {
-  const [selectedDate, setSelectedDate] = useState(null);
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  }
-
-
-
   return (
     <div className="App">
-
-      <Calendrier onChange={handleDateChange} />
-      {selectedDate && <Reservation selectedDate={selectedDate} />}
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendrier/>
     </div>
   );
 }
--- react-app/src/Calendrier.js
+++ react-app/src/Calendrier.js
@@ -1,24 +1,64 @@
 import Calendar from 'react-calendar';
-import {useState} from "react"
+import { useEffect, useState } from "react";
 
 const Calendrier = () => {
-  const[date,setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
+  const [reservations, setReservations] = useState(null);
 
   const onChange = date => {
     setDate(date);
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    console.log("Year:", year);
-    console.log("Month:", month);
-    console.log("Day:", day);
   }
 
-  return(
+  useEffect(() => {
+    if (date) {
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      console.log("Year: ", year);
+      console.log("Month: ", month);
+      console.log("Day: ", day);
+
+      //const url = `api/employe/dayreservations/${year}/${month}/${day}`;
+      const url = `http://localhost:5000/api/employe/dayreservations/${year}/${month}/${day}`;
+
+      fetch(url)
+        .then(res => res.json())
+        .then(data => setReservations(data))
+    }
+  }, [date]);
+
+  return (
     <div>
-      <Calendar onChange={onChange} value={date}/>
+      <Calendar onChange={onChange} value={date} />
+      {reservations && reservations.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Num de reservation</th>
+              <th>Debut</th>
+              <th>Fin</th>
+              <th>Num de table</th>
+              <th>Nb de Sieges</th>
+              <th>Specifications</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reservations.map((reservation) => (
+              <tr key={reservation.numero_res}>
+                <td>{reservation.numero_res}</td>
+                <td>{reservation.heure_debut}</td>
+                <td>{reservation.heure_fin}</td>
+                <td>{reservation.numero_table}</td>
+                <td>{reservation.nb_sieges}</td>
+                <td>{reservation.specification}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) :
+        <div> Aucune reservation pour cette date</div>}
     </div>
-  )
+  );
 }
 
 export default Calendrier;
