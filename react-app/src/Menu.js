5d9234eee637d457ba4f29c0cee2a2b310d00699
Cory Alejandra Herrera Gallego <corygallego2015@gmail.com>
2024-04-12 10:59:05 -0400

    debut menu client

--- react-app/src/Menu.js
+++ react-app/src/Menu.js
@@ -1,8 +1,17 @@
+import React from 'react';
+
 const Menu = () => {
+    const data = []//FETCH ITEMS DU MENU
     return ( 
         //html du menu ici vvv
         <div className="Menu">
-
+            <h1>MENU</h1>
+            <ul>
+                {data.map((item, index)=>(
+                    <li key= {index}> {item} </li>
+                ))}
+            </ul>
+            <button>Retour</button>
         </div>
      );
 }
